b32522693600b6dee9d877d9d50362814cac15a0
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-08-07 18:21:19 -0300

    put delete and get

--- routes/grades.js
+++ routes/grades.js
@@ -1,16 +1,26 @@
 import express from 'express';
-import { promises as fs } from 'fs';
+import { promises as fs, write } from 'fs';
 
 const { readFile, writeFile } = fs;
 const router = express.Router();
+
+function valida(grade) {
+  if (!grade.student || !grade.subject || !grade.type || grade.value == null) {
+    throw new Error('Algum campo está em branco');
+  }
+}
+
+function validaIndex(index) {
+  if (index === -1) {
+    throw new Error('Registro não encontrado');
+  }
+}
 
 router.post('/', async (req, res, next) => {
   try {
     let grade = req.body;
 
-    if (!grade.student || !grade.subject || !grade.type || !grade.value) {
-      throw new Error('Algum campo está em branco');
-    }
+    valida(grade);
 
     const data = JSON.parse(await readFile(global.fileName));
 
@@ -27,6 +37,67 @@ router.post('/', async (req, res, next) => {
 
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
+    console.log(grade);
+    res.send(grade);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/', async (req, res, next) => {
+  try {
+    const grade = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.grades.findIndex((a) => a.id === grade.id);
+
+    valida(grade);
+    validaIndex(index);
+
+    data.grades[index].student = grade.student;
+    data.grades[index].subject = grade.subject;
+    data.grades[index].type = grade.type;
+    data.grades[index].value = grade.value;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    console.log(grade);
+    res.send(grade);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.grades.findIndex(
+      (a) => a.id === parseInt(req.params.id)
+    );
+
+    validaIndex(index);
+
+    data.grades = data.grades.filter(
+      (grade) => grade.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    console.log('Conta ' + req.params.id + ' removida com sucesso');
+    res.send('Conta ' + req.params.id + ' removida com sucesso');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.grades.findIndex(
+      (a) => a.id === parseInt(req.params.id)
+    );
+
+    validaIndex(index);
+
+    const grade = data.grades.find(
+      (grade) => grade.id === parseInt(req.params.id)
+    );
     console.log(grade);
     res.send(grade);
   } catch (err) {
