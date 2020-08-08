e2015045ec57094448209eb34ea0ec9e6f3c27d5
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-08-08 13:27:03 -0300

    finishing-project

--- routes/grades.js
+++ routes/grades.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { promises as fs, write } from 'fs';
+import { nextTick } from 'process';
 
 const { readFile, writeFile } = fs;
 const router = express.Router();
@@ -12,6 +13,12 @@ function valida(grade) {
 
 function validaIndex(index) {
   if (index === -1) {
+    throw new Error('Registro não encontrado');
+  }
+}
+
+function validaParams(studentGrades) {
+  if (studentGrades.length === 0) {
     throw new Error('Registro não encontrado');
   }
 }
@@ -100,6 +107,93 @@ router.get('/:id', async (req, res, next) => {
     );
     console.log(grade);
     res.send(grade);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/total/:student/:subject', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const studentGrades = data.grades.filter(
+      (student) =>
+        student.student === req.params.student &&
+        student.subject === req.params.subject
+    );
+    validaParams(studentGrades);
+    const studentTotalGrade = studentGrades.reduce((acc, cur) => {
+      return acc + cur.value;
+    }, 0);
+
+    console.log(
+      'A nota total do ' +
+        req.params.student +
+        ' em ' +
+        req.params.subject +
+        ' foi: ' +
+        studentTotalGrade
+    );
+    res.send(
+      'A nota total do ' +
+        req.params.student +
+        ' em ' +
+        req.params.subject +
+        ' foi: ' +
+        studentTotalGrade
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/media/:subject/:type', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const studentGrades = data.grades.filter(
+      (student) =>
+        student.subject === req.params.subject &&
+        student.type === req.params.type
+    );
+    validaParams(studentGrades);
+    const studentMediaGrade = studentGrades.reduce((acc, cur) => {
+      return acc + cur.value / studentGrades.length;
+    }, 0);
+    console.log(
+      'A media da matéria ' +
+        req.params.subject +
+        ' no exercício ' +
+        req.params.type +
+        ' foi: ' +
+        studentMediaGrade
+    );
+    res.send(
+      'A media da matéria ' +
+        req.params.subject +
+        ' no exercício ' +
+        req.params.type +
+        ' foi: ' +
+        studentMediaGrade
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/best/:subject/:type', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const studentGrades = data.grades.filter(
+      (student) =>
+        student.subject === req.params.subject &&
+        student.type === req.params.type
+    );
+    const studentSort = studentGrades
+      .sort((a, b) => {
+        return b.value - a.value;
+      })
+      .slice(0, 3);
+    validaParams(studentGrades);
+    res.send(studentSort);
   } catch (err) {
     next(err);
   }
